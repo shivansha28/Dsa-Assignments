a904a9bdc6e16ecc4c355943eaf64719bb9cc143
shivansha28 <132441598+shivansha28@users.noreply.github.com>
2023-05-03 22:28:41 +0530

    Create FourTickets.cpp

---
+++ Code chef/FourTickets.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int X;
+	    cin>>X;
+	    if((4*X)<=1000){
+	        cout<<"YES"<<endl;
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	}
+	return 0;
+}
