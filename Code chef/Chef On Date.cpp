56aa88f3c3016bb82e0db0ccf6e2dbd553711df6
shivansha28 <132441598+shivansha28@users.noreply.github.com>
2023-05-03 21:52:29 +0530

    Create Chef On Date.cpp

---
+++ Code chef/Chef On Date.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int X,Y;
+	    cin>>X>>Y;
+	    if(X>=Y){
+	        cout<<"YES"<<endl;
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	}
+	return 0;
+}
